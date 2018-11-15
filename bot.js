7d9eb14f60ffa3f28e287b2ffbc6c21bb5c9a19b
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-11-15 18:48:25 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1085,7 +1085,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`.help | Golden Server`,"http://twitch.tv/Death Shop")
+client.user.setGame(`.help  Golden Server`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
